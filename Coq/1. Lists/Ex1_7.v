9e3cd24fc04a6674339700334806ea08c6a2ff66
SuperDuckDuck <SvenWille@users.noreply.github.com>
2017-11-07 04:22:32 +0100

    empty message

---
+++ Coq/1. Lists/Ex1_7.v
@@ -0,0 +1,12 @@
+Require Import List.
+Import ListNotations.
+Require Import ListSet.
+Require Import EquivDec.
+
+Fixpoint list_union {X : Type} `{EqDec X}(ls ys : list X): list X := 
+match ls with 
+| [] => ys 
+| x::xs => let tmp := list_union xs ys in 
+  if (existsb (fun y => equiv_decb x y) tmp) then tmp else x :: tmp
+end.
+
