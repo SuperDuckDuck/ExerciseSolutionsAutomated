495ac849eb6e130b30140590eeb244b155b43f30
SuperDuckDuck <SvenWille@users.noreply.github.com>
2017-11-17 10:11:50 +0100

    empty message

---
+++ Coq/2. Trees and other inductive data types/Ex2_1.v
@@ -0,0 +1,61 @@
+Require Import List.
+Import ListNotations.
+
+
+Inductive tree (X : Type) :=
+| Leaf : X -> tree X
+| Node : tree X -> X -> tree X -> tree X.
+
+
+Arguments Leaf {X} _.
+Arguments Node {X} _ _ _.
+
+Fixpoint preOrder {X : Type} (t : tree X): list X := 
+match t with 
+| Leaf val => [val]
+| Node l val r => val :: preOrder l ++ preOrder r
+end.
+
+Fixpoint postOrder {X  : Type} (t : tree X) : list X :=
+match t with 
+| Leaf val => [val]
+| Node l val r => postOrder l ++ postOrder r ++ [val]
+end.
+
+Fixpoint inOrder {X : Type}(t : tree X) : list X :=
+match t with 
+| Leaf val => [val]
+| Node l val r => inOrder l ++ val :: inOrder r 
+end.
+
+Fixpoint mirror {X : Type} (t : tree X) : tree X :=
+match t with 
+| Node l val r => Node (mirror r) val (mirror l)
+| tr => tr
+end.
+
+Lemma pre_pre_counterExample : 
+  let t := Node (Leaf 2) 1 (Leaf 3) in preOrder (mirror t) <> rev (preOrder t).
+Proof.
+  intro.
+  intro.
+  unfold t in H. discriminate.
+Qed.
+
+Lemma pre_post {X : Type} (t : tree X): preOrder (mirror t) = rev (postOrder t).
+Proof.
+  induction t.
+  + reflexivity.
+  + simpl. rewrite rev_app_distr. rewrite rev_app_distr.
+    rewrite IHt1. simpl. rewrite IHt2. reflexivity.
+Qed.
+
+Lemma post_pre {X : Type} (t : tree X): postOrder (mirror t) = rev (preOrder t).
+Proof.
+  induction t.
+  + reflexivity.
+  + simpl. rewrite rev_app_distr. rewrite IHt1. rewrite IHt2. intuition.
+Qed.
+
+
+
