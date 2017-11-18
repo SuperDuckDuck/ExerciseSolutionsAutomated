fcd3273e83d3985e57fc07dbfefb897fbdfe1a9e
SuperDuckDuck <SvenWille@users.noreply.github.com>
2017-11-18 10:56:53 +0100

    empty message

--- Coq/2. Trees and other inductive data types/Ex2_1.v
+++ Coq/2. Trees and other inductive data types/Ex2_1.v
@@ -58,4 +58,58 @@ Proof.
 Qed.
 
 
+Lemma pre_in_counterExample: 
+  let t := Node (Leaf 2) 1 (Leaf 3) in preOrder (mirror t) <> rev (inOrder t).
+Proof.
+  intro.
+  intro.
+  unfold t in H.
+  discriminate.
+Qed.
+
+Lemma in_pre_counterexample : 
+  let t := Node (Leaf 2) 1 (Leaf 3) in inOrder (mirror t) <> rev (preOrder t).
+Proof.
+  intro.
+  intro. 
+  unfold t in H.
+  discriminate.
+Qed.
+
+Lemma post_post_counterexample :
+  let t := Node (Leaf 2) 1 (Leaf 3) in postOrder (mirror t) <> rev (postOrder t).
+Proof.
+  intro.
+  intro.
+  unfold t in H.
+  discriminate.
+Qed.
+
+Lemma post_in_counterexample :
+  let t := Node (Leaf 2) 1 (Leaf 3) in postOrder (mirror t) <> rev (inOrder t).
+Proof.
+  intro.
+  intro.
+  discriminate.
+Qed.
+
+Lemma in_post_counterexample : 
+  let  t := Node (Leaf 2) 1 (Leaf 3) in inOrder (mirror t) <> rev (postOrder t).
+Proof.
+  intro.
+  intro.
+  discriminate.
+Qed.
+
+Lemma in_in {X : Type} (t : tree X):
+  inOrder (mirror t) = rev (inOrder t).
+Proof.
+  induction t.
+  + reflexivity.
+  + simpl. rewrite rev_app_distr. simpl. rewrite <- IHt1. rewrite <- IHt2. 
+    rewrite <- app_assoc. reflexivity.
+Qed.
+
+
+
 
--- SSReflect/Ex1_1.v
+++ SSReflect/Ex1_1.v
@@ -4,5 +4,20 @@ From Coq Require Import ssreflect ssrfun ssrbool.
 Set Implicit Arguments. Unset Strict Implicit. Unset Printing Implicit Defensive.
 
 
+Fixpoint snoc {X : Type}(ls : list X)(val : X) : list X :=
+if ls is cons x xs then x :: snoc xs  val else [val].
 
 
+Lemma snoc_app {X : Type}(val : X)(xs ys : list X): snoc (xs ++ ys) val = xs ++ snoc ys val.
+Proof.
+  elim : xs => [| x xs IH].
+  + by [].
+  + simpl. rewrite {} IH. by [].
+Qed.
+
+Theorem rev_cons {X : Type}(x : X)(xs : list X): rev (x :: xs) = snoc (rev xs) x.
+Proof.
+  elim : xs => [| val xs IH].
+  + by []. 
+  + simpl. rewrite snoc_app. simpl. rewrite <- app_assoc. by [].
+Qed.
